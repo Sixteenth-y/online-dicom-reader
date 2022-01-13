d625401f72397796f0677f7d847fa6a18030a7a6
Sixteenth-y <577899820@qq.com>
2022-01-13 18:07:00 +0800

    01/13/2022 配置文件

---
+++ src/main/java/com/dicomreader/DicomReaderApplication.java
@@ -0,0 +1,13 @@
+package com.dicomreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicomReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicomReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dicomreader/DicomReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicomreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicomReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
